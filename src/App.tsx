d3a3645dc6b2544886802c1bc48884853f789863
Simon Weller <s.meierbeck@gmail.com>
2020-06-04 22:05:09 +0200

    get initial test to pass

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,73 @@
-import React from 'react';
+import React from "react";
 
 function App() {
-    return (
-        <>
-            <section className="todoapp">
-                <header className="header">
-                    <h1>todos</h1>
-                    <input className="new-todo" placeholder="What needs to be done?" autoFocus/>
-                </header>
-                <section className="main">
-                    <input id="toggle-all" className="toggle-all" type="checkbox"/>
-                    <label htmlFor="toggle-all">Mark all as complete</label>
-                    <ul className="todo-list">
-                        <li className="completed">
-                            <div className="view">
-                                <input className="toggle" type="checkbox" checked/>
-                                <label>Taste JavaScript</label>
-                                <button className="destroy"/>
-                            </div>
-                            <input className="edit" value="Create a TodoMVC template"/>
-                        </li>
-                        <li>
-                            <div className="view">
-                                <input className="toggle" type="checkbox"/>
-                                <label>Buy a unicorn</label>
-                                <button className="destroy"/>
-                            </div>
-                            <input className="edit" value="Rule the web"/>
-                        </li>
-                    </ul>
-                </section>
-                <footer className="footer">
-                    <span className="todo-count"><strong>0</strong> item left</span>
-                    <ul className="filters">
-                        <li>
-                            <a className="selected" href="#/">All</a>
-                        </li>
-                        <li>
-                            <a href="#/active">Active</a>
-                        </li>
-                        <li>
-                            <a href="#/completed">Completed</a>
-                        </li>
-                    </ul>
-                    <button className="clear-completed">Clear completed</button>
-                </footer>
-            </section>
-            <footer className="info">
-                <p>Double-click to edit a todo</p>
-                <p>Template by <a href="http://sindresorhus.com">Sindre Sorhus</a></p>
-                <p>Created by <a href="https://www.sweller.de">Simon Weller</a></p>
-                <p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
-            </footer>
-        </>
-    );
+  return (
+    <>
+      <section className="todoapp">
+        <header className="header">
+          <h1>todos</h1>
+          <input
+            className="new-todo"
+            placeholder="What needs to be done?"
+            autoFocus
+          />
+        </header>
+        <section className="main" style={{ display: "none" }}>
+          <input id="toggle-all" className="toggle-all" type="checkbox" />
+          <label htmlFor="toggle-all">Mark all as complete</label>
+          <ul className="todo-list">
+            <li className="completed">
+              <div className="view">
+                <input className="toggle" type="checkbox" checked />
+                <label>Taste JavaScript</label>
+                <button className="destroy" />
+              </div>
+              <input className="edit" value="Create a TodoMVC template" />
+            </li>
+            <li>
+              <div className="view">
+                <input className="toggle" type="checkbox" />
+                <label>Buy a unicorn</label>
+                <button className="destroy" />
+              </div>
+              <input className="edit" value="Rule the web" />
+            </li>
+          </ul>
+        </section>
+        <footer className="footer" style={{ display: "none" }}>
+          <span className="todo-count">
+            <strong>0</strong> item left
+          </span>
+          <ul className="filters">
+            <li>
+              <a className="selected" href="#/">
+                All
+              </a>
+            </li>
+            <li>
+              <a href="#/active">Active</a>
+            </li>
+            <li>
+              <a href="#/completed">Completed</a>
+            </li>
+          </ul>
+          <button className="clear-completed">Clear completed</button>
+        </footer>
+      </section>
+      <footer className="info">
+        <p>Double-click to edit a todo</p>
+        <p>
+          Template by <a href="http://sindresorhus.com">Sindre Sorhus</a>
+        </p>
+        <p>
+          Created by <a href="https://www.sweller.de">Simon Weller</a>
+        </p>
+        <p>
+          Part of <a href="http://todomvc.com">TodoMVC</a>
+        </p>
+      </footer>
+    </>
+  );
 }
 
 export default App;
