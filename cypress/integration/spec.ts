0b29b490704eb94e64ac10dc5bbbd6687ec78bad
Simon Weller <s.meierbeck@gmail.com>
2020-06-05 19:44:21 +0200

    add failing test for adding new todo

--- cypress/integration/spec.ts
+++ cypress/integration/spec.ts
@@ -10,6 +10,18 @@ describe(`New todo`, () => {
   it(`should focus the input element when the page is loaded`, () => {
     cy.get(".new-todo").should("have.focus");
   });
-  it(`should create the todo, append it to the todo list, and clear the input when enter is pressed`, () => {});
+
+  it(`should create the todo, append it to the todo list, and clear the input when enter is pressed`, () => {
+    cy.get(".new-todo").type("Learn javascript").type("{enter}");
+    cy.get(".todo-list").find("li").should("have.length", 1);
+    cy.get(".todo-list")
+      .find("li")
+      .first()
+      .should("contain", "Learn javascript");
+    cy.get(".new-todo").should("have.value", "");
+    cy.get(".main").should("be.visible");
+    cy.get(".footer").should("be.visible");
+  });
+
   it(`should not create a todo if the text is empty after trimming`, () => {});
 });
