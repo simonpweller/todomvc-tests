cafd0aa9afa936faf41ba1735047805ca80fbb59
Simon Weller <s.meierbeck@gmail.com>
2020-06-11 14:50:58 +0200

    tweak 'No todos' to allow removing the elements from the DOM entirely, rather than requiring them to be hidden

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,8 +1,8 @@
 describe(`No todos`, () => {
   it(`should hide #main and #footer if there are no todos`, () => {
     cy.visit("/");
-    cy.get(".main").should("be.hidden");
-    cy.get(".footer").should("be.hidden");
+    cy.get(".main").should("not.be.visible");
+    cy.get(".footer").should("not.be.visible");
   });
 });
 
